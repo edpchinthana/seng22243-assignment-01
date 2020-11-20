09e2eacc59ea07e6e3c28b4e90aeabf5479da072
edpchinthana <edpchinthana@outlook.com>
2020-11-20 22:11:28 +0530

    permission issues fixed

--- app/src/main/java/com/example/myblogs/MainActivity.kt
+++ app/src/main/java/com/example/myblogs/MainActivity.kt
@@ -7,10 +7,12 @@ import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myblogs.models.Post
 import com.example.myblogs.repositories.PostRepository
+import com.google.gson.GsonBuilder
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,8 +21,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val retrofit = Retrofit.Builder().baseUrl("https://jsonplaceholder.typicode.com/").build()
+        val gson = GsonBuilder().create()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
         val postAPI: PostRepository = retrofit.create<PostRepository>(PostRepository::class.java)
         var postCall = postAPI.posts
         postCall.enqueue(object : Callback<List<Post>>{
@@ -29,9 +34,9 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
-                //val listView = findViewById<ListView>(R.id.list_view)
-                //val adapter =  PostAdapter(this@MainActivity, response.body() as ArrayList<Post>)
-                //listView.adapter = adapter
+                val listView = findViewById<ListView>(R.id.list_view)
+                val adapter =  PostAdapter(this@MainActivity, response.body() as ArrayList<Post>)
+                listView.adapter = adapter
                 println("data recieved")
             }
 
@@ -42,4 +47,5 @@ class MainActivity : AppCompatActivity() {
 
 
 }
+
 
