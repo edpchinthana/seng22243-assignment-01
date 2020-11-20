092aea85b214f18a9d0a57eed353b8300abce02b
edpchinthana <edpchinthana@outlook.com>
2020-11-20 21:09:13 +0530

    sample list view

--- app/src/main/java/com/example/myblogs/MainActivity.kt
+++ app/src/main/java/com/example/myblogs/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.myblogs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import com.example.myblogs.models.Post
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val postList = mutableListOf<Post>()
+        val post = Post(1,1,"title","body")
+        postList.add(post)
+        val listView = findViewById<ListView>(R.id.list_view)
+        val adapter =  ArrayAdapter<Post>(this, android.R.layout.simple_list_item_1, postList)
+        listView.adapter = adapter
     }
+
+
+
+
 }
