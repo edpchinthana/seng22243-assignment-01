fa9ba0094b6ba91fad11a1c6d2cf0f60e1952996
edpchinthana <edpchinthana@outlook.com>
2020-11-20 21:54:05 +0530

    custom list item properties changed

--- app/src/main/java/com/example/myblogs/MainActivity.kt
+++ app/src/main/java/com/example/myblogs/MainActivity.kt
@@ -1,25 +1,48 @@
 package com.example.myblogs
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.telephony.ims.RegistrationManager
+import android.util.Log
 import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myblogs.models.Post
+import com.example.myblogs.repositories.PostRepository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val postList = mutableListOf<Post>()
-        val post = Post(1,1,"title","body")
-        postList.add(post)
-        val listView = findViewById<ListView>(R.id.list_view)
-        val adapter =  PostAdapter(this, postList as ArrayList<Post>)
-        listView.adapter = adapter
+
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com")
+            .build()
+        val postAPI: PostRepository = retrofit.create<PostRepository>(PostRepository::class.java)
+        var postCall = postAPI.posts
+        val context = this
+        postCall.enqueue(object : Callback<List<Post>>{
+            override fun onFailure(call: Call<List<Post>>, t: Throwable) {
+                Log.d("GET api",t.message.toString())
+            }
+
+            override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+                val listView = findViewById<ListView>(R.id.list_view)
+                //val adapter =  PostAdapter(context, response as ArrayList<Post>)
+                //listView.adapter = adapter
+                println("data recieved")
+            }
+
+        })
     }
 
 
 
 
 }
+
--- app/src/main/java/com/example/myblogs/PostAdapter.kt
+++ app/src/main/java/com/example/myblogs/PostAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
 import com.example.myblogs.models.Post
+import retrofit2.Callback
 
 class PostAdapter(private val context: Context, private val dataSource: ArrayList<Post>): BaseAdapter() {
     private val inflater: LayoutInflater
